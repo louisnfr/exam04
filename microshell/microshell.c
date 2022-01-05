f2e478e1f3a0bd42ab9d8d1a9a27166d19674c9f
Louis Raffin <louis.raffin@gmail.com>
2022-01-05 19:24:50 +0100

    exam04

--- microshell/microshell.c
+++ microshell/microshell.c
@@ -33,7 +33,7 @@ int	ft_strlen(char *str)
 	return (i);
 }
 
-int	show_error(char const *str)
+int	show_error(char *str)
 {
 	if (str)
 		write(STDERR, str, ft_strlen(str));
@@ -49,12 +49,12 @@ int	exit_fatal(void)
 
 void	*exit_fatal_ptr(void)
 {
-	show_error();
+	exit_fatal();
 	exit(EXIT_FAILURE);
 	return (NULL);
 }
 
-char	*ft_strdup(char const *str)
+char	*ft_strdup(char *str)
 {
 	char	*copy;
 	int		i;
@@ -90,4 +90,165 @@ int	add_arg(t_list *cmd, char *arg)
 	return (EXIT_SUCCESS);
 }
 
+int	list_push(t_list **list, char *arg)
+{
+	t_list *new;
 
+	new = malloc(sizeof(t_list));
+	if (!new)
+		return (exit_fatal());
+	new->args = NULL;
+	new->length = 0;
+	new->type = TYPE_END;
+	new->previous = NULL;
+	new->next = NULL;
+	if (*list)
+	{
+		(*list)->next = new;
+		new->previous = *list;
+	}
+	*list = new;
+	return (add_arg(new, arg));
+}
+
+int	list_rewind(t_list **list)
+{
+	while (*list && (*list)->previous)
+		*list = (*list)->previous;
+	return (EXIT_SUCCESS);
+}
+
+int	list_clear(t_list **cmds)
+{
+	t_list	*tmp;
+	int		i;
+
+	list_rewind(cmds);
+	while (*cmds)
+	{
+		tmp = (*cmds)->next;
+		i = 0;
+		while (i < (*cmds)->length)
+			free((*cmds)->args[i++]);
+		free((*cmds)->args);
+		free(*cmds);
+		*cmds = tmp;
+	}
+	*cmds = NULL;
+	return (EXIT_SUCCESS);
+}
+
+int	parse_arg(t_list **cmds, char *arg)
+{
+	int	is_break;
+
+	is_break = (strcmp(";", arg) == 0);
+	if (is_break && !*cmds)
+		return (EXIT_SUCCESS);
+	else if (!is_break && (!*cmds || (*cmds)->type > TYPE_END))
+		return (list_push(cmds, arg));
+	else if (strcmp("|", arg) == 0)
+		(*cmds)->type = TYPE_PIPE;
+	else if (is_break)
+		(*cmds)->type = TYPE_BREAK;
+	else
+		return (add_arg(*cmds, arg));
+	return (EXIT_SUCCESS);
+}
+
+int	exec_cmd(t_list *cmd, char **env)
+{
+	pid_t	pid;
+	int		ret;
+	int		status;
+	int		pipe_open;
+
+	ret = EXIT_FAILURE;
+	pipe_open = 0;
+	if (cmd->type == TYPE_PIPE || (cmd->previous && cmd->previous->type == TYPE_PIPE))
+	{
+		pipe_open = 1;
+		if (pipe(cmd->pipes))
+			return (exit_fatal());
+	}
+	pid = fork();
+	if (pid < 0)
+		return (exit_fatal());
+	else if (pid == 0)
+	{
+		if (cmd->type == TYPE_PIPE && dup2(cmd->pipes[SIDE_IN], STDOUT) < 0)
+			return (exit_fatal());
+		if (cmd->previous && cmd->previous->type == TYPE_PIPE && dup2(cmd->previous->pipes[SIDE_OUT], STDIN) < 0)
+			return (exit_fatal());
+		if ((ret = execve(cmd->args[0], cmd->args, env)) < 0)
+		{
+			show_error("error: cannot execute ");
+			show_error(cmd->args[0]);
+			show_error("\n");
+		}
+		exit(ret);
+	}
+	else
+	{
+		waitpid(pid, &status, 0);
+		if (pipe_open)
+		{
+			close(cmd->pipes[SIDE_IN]);
+			if (!cmd->next || cmd->type == TYPE_BREAK)
+				close(cmd->pipes[SIDE_OUT]);
+		}
+		if (cmd->previous && cmd->previous->type == TYPE_PIPE)
+			close(cmd->previous->pipes[SIDE_OUT]);
+		if (WIFEXITED(status))
+			ret = WEXITSTATUS(status);
+	}
+	return (ret);
+}
+
+int	exec_cmds(t_list **cmds, char **env)
+{
+	t_list	*crt;
+	int		ret;
+
+	ret = EXIT_SUCCESS;
+	list_rewind(cmds);
+	while (*cmds)
+	{
+		crt = *cmds;
+		if (!strcmp("cd", crt->args[0]))
+		{
+			ret = EXIT_SUCCESS;
+			if (crt->length < 2)
+				ret = show_error("error: cd: bad arguments\n");
+			else if (chdir(crt->args[1]))
+			{
+				ret = show_error("error: cd: cannot change directory to ");
+				show_error(crt->args[1]);
+				show_error("\n");
+			}
+		}
+		else
+			ret = exec_cmd(crt, env);
+		if (!(*cmds)->next)
+			break;
+		*cmds = (*cmds)->next;
+	}
+	return (ret);
+}
+
+int	main(int ac, char **av, char **env)
+{
+	t_list	*cmds;
+	int		ret;
+	int		i;
+
+	cmds = NULL;
+	ret = EXIT_SUCCESS;
+	i = 1;
+	while (i < ac)
+		parse_arg(&cmds, av[i++]);
+	if (cmds)
+		ret = exec_cmds(&cmds, env);
+	list_clear(&cmds);
+	return (ret);
+}
